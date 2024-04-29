79adda1fdbdd688700dc9624ab88f782c07cf2ae
Kabuto4dev <100389319+Kabuto4dev@users.noreply.github.com>
2024-04-29 19:17:54 +0200

    correct lab mysql server#1

--- seeding.sql
+++ seeding.sql
@@ -1,68 +1,3 @@
--- Challenge 1 - Design the Database + Challenge 2 - Create the Database and Tables
-CREATE DATABASE IF NOT EXISTS lab_mysql;
-
-USE lab_mysql;
-
-CREATE TABLE cars (
-id INT AUTO_INCREMENT NOT NULL,
-vin CHAR (17) NOT NULL,
-manufacturer VARCHAR (50) NOT NULL,
-model VARCHAR (50) NOT NULL,
-years YEAR,
-color VARCHAR (200),
-PRIMARY KEY (id)
-);
-
-CREATE TABLE customers (
-id INT AUTO_INCREMENT NOT NULL,
-cust_id CHAR (5) NOT NULL,
-cust_name VARCHAR (50) NOT NULL,
-cust_phone VARCHAR (12) NOT NULL,
-cust_email VARCHAR (12) NOT NULL,
-cust_address VARCHAR (300) NOT NULL,
-cust_city VARCHAR (300) NOT NULL,
-cust_state VARCHAR (300) NOT NULL,
-cust_country VARCHAR (300) NOT NULL,
-cust_zipcode CHAR(10) NOT NULL,
-PRIMARY KEY (id)
-);
-
-CREATE TABLE salespersons (
-id INT AUTO_INCREMENT NOT NULL,
-staff_id CHAR (6) NOT NULL,
-sales_name VARCHAR (100) NOT NULL,
-store VARCHAR (300) NOT NULL,
-PRIMARY KEY (id)
-);
-
-CREATE TABLE invoices (
-id INT AUTO_INCREMENT NOT NULL,
-invoice_number CHAR (50) NOT NULL,
-invoice_date VARCHAR (50) NOT NULL,
-car INT NOT NULL,
-customer INT NOT NULL,
-salesperson INT NOT NULL,
-PRIMARY KEY (id),
-	FOREIGN KEY (car)
-	REFERENCES cars (id) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY (customer)
-	REFERENCES customers (id) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY (salesperson)
-	REFERENCES salespersons (id) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-SELECT *
-FROM cars;
-
-SELECT *
-FROM customers;
-
-SELECT *
-FROM salespersons;
-
-SELECT *
-FROM invoices;
-
 -- Challenge 3 - Seeding the Database
 
 INSERT INTO cars (vin, manufacturer, model, years, color)
@@ -89,9 +24,9 @@ VALUES ('00001','Petey Cruiser','Madrid'),
         ('00008','Shonda Leer','São Paulo');
         
 INSERT INTO invoices (invoice_number, invoice_date, car, customer,salesperson)
-VALUES ('852399038','20180822',19,10,3),
-		('731166526','20181231',20,11,5),
-		('271135104','20190122',21,12,7);
+VALUES ('852399038','20180822',1,1,3),
+		('731166526','20181231',3,3,5),
+		('271135104','20190122',2,2,7);
  
 
  
